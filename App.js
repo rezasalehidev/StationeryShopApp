c0b18eca36c7e08e69299745762da99978009bd5
rezasalehi <rezasalehidev@gmail.com>
2021-11-12 15:17:37 +0330

    Init Project

---
+++ App.js
@@ -0,0 +1,20 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import DrawerNavigator from './src/views/navigators/DrawerNavigator';
+import DetailsScreen from './src/views/screens/DetailsScreen';
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="HomeScreen" component={DrawerNavigator} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/const/Stationery.js
@@ -0,0 +1,87 @@
+const Stationery = [
+  {
+    name: 'Pens',
+    names: [
+      {
+        id: '1',
+        name: 'Pen1',
+        image: require('../assets/eraser1.png'),
+        type: 'eraser',
+        prety: '5 score',
+      },
+      {
+        id: '2',
+        name: 'Pen2',
+        image: require('../assets/eraser2.png'),
+        type: 'eraser',
+        prety: '4 score',
+      },
+      {
+        id: '3',
+        name: 'Pen3',
+        image: require('../assets/eraser3.png'),
+        type: 'eraser',
+        prety: '3 score',
+      },
+    ],
+  },
+  {
+    name: 'pencile',
+    names: [
+      {
+        id: '1',
+        name: 'pencile1',
+        image: require('../assets/pencile1.png'),
+        type: 'pencile',
+        prety: '5 score',
+      },
+      {
+        id: '2',
+        name: 'pencile2',
+        image: require('../assets/pencile2.png'),
+        type: 'pencile',
+        prety: '2 score',
+      },
+    ],
+  },
+  {
+    name: 'pen',
+    names: [
+      {
+        id: '1',
+        name: 'pen1',
+        image: require('../assets/pen1.png'),
+        type: 'pen',
+        prety: '3 score',
+      },
+      {
+        id: '2',
+        name: 'pen',
+        image: require('../assets/pen2.png'),
+        type: 'pen',
+        prety: '5 score',
+      },
+    ],
+  },
+  {
+    name: 'compas',
+    names: [
+      {
+        id: '1',
+        name: 'compas1',
+        image: require('../assets/compas1.png'),
+        type: 'compas',
+        age: '5 score',
+      },
+      {
+        id: '2',
+        name: 'compas2',
+        image: require('../assets/compas2.png'),
+        type: 'compas',
+        prety: '4 score',
+      },
+    ],
+  },
+];
+
+export default Stationery;
